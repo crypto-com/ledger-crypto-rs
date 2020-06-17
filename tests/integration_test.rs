86360af186719e1c7a26166a335d2d63fdf0e496
Juan Leni <lenijuan@gmail.com>
2020-06-17 21:32:57 +0200

    activate transfer tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -58,28 +58,28 @@ mod integration_tests {
         assert!(resp.major == 0);
     }
 
-    // #[async_test]
-    // #[serial]
-    // async fn address_transfer() {
-    //     init_logging();
-    //
-    //     let transport = APDUTransport {
-    //         transport_wrapper: ledger::TransportNativeHID::new().unwrap(),
-    //     };
-    //     let app = CryptoApp::new(transport);
-    //
-    //     let path = BIP44Path::from_string("m/44'/394'/0'/0/0").unwrap();
-    //     let resp = app.get_address(&path, false).await.unwrap();
-    //
-    //     assert_eq!(resp.public_key.len(), 65);
-    //
-    //     let pkhex = hex::encode(&resp.public_key[..]);
-    //     println!("Public Key   {:?}", pkhex);
-    //     println!("Address address {:?}", resp.address);
-    //
-    //     assert_eq!(pkhex, "");
-    //     assert_eq!(resp.address, "");
-    // }
+    #[async_test]
+    #[serial]
+    async fn address_transfer() {
+        init_logging();
+
+        let transport = APDUTransport {
+            transport_wrapper: ledger::TransportNativeHID::new().unwrap(),
+        };
+        let app = CryptoApp::new(transport);
+
+        let path = BIP44Path::from_string("m/44'/394'/0'/0/0").unwrap();
+        let resp = app.get_address(&path, false).await.unwrap();
+
+        assert_eq!(resp.public_key.len(), 65);
+
+        let pkhex = hex::encode(&resp.public_key[..]);
+        println!("Public Key   {:?}", pkhex);
+        println!("Address address {:?}", resp.address);
+
+        assert_eq!(pkhex, "048ef50054db1b8c5ff9b02640a25463a37ca7d4249da43b4e6f4ea8f7af70daec5e276294642dec9dc28079397d6962cc32d3909e92995167768fbde7250424d9");
+        assert_eq!(resp.address, "cro1n97t35jymgksmh73mh0zj3qx539k3hg4pfhmncake4ssm3z7rreqzkza53");
+    }
 
     #[async_test]
     #[serial]
@@ -104,32 +104,31 @@ mod integration_tests {
             pkhex,
             "04eda422888bff3b3fa957ab9a509b6ae70c1249c9b9b35f1832aeb2e9a4f94b86076054d2641464e55f85e0c6d27d7dcebd60386f6178dec5e77a2a03330952aa"
         );
-        assert_eq!(resp.address, "D218B4DE1EF21BE0894BB008F3C7D0D3439C174B");
+        assert_eq!(resp.address, "f3c7d0d3439c174b9ce8178c2d2ea95dc1f45c28");
     }
 
-    // ASYNC TEST DOES NOT ALLOW ignore ATTRIBUTE
-    // #[async_test]
-    // #[serial]
-    // async fn show_address_transfer() {
-    //     init_logging();
-    //
-    //     let transport = APDUTransport {
-    //         transport_wrapper: ledger::TransportNativeHID::new().unwrap(),
-    //     };
-    //     let app = CryptoApp::new(transport);
-    //
-    //     let path = BIP44Path::from_string("m/44'/394'/0/0/0").unwrap();
-    //     let resp = app.get_address(&path, true).await.unwrap();
-    //
-    //     assert_eq!(resp.public_key.len(), 65);
-    //
-    //     let pkhex = hex::encode(&resp.public_key[..]);
-    //     println!("Public Key   {:?}", pkhex);
-    //     println!("Address address {:?}", resp.address);
-    //
-    //     assert_eq!(pkhex, "");
-    //     assert_eq!(resp.address, "");
-    // }
+    #[async_test]
+    #[serial]
+    async fn show_address_transfer() {
+        init_logging();
+
+        let transport = APDUTransport {
+            transport_wrapper: ledger::TransportNativeHID::new().unwrap(),
+        };
+        let app = CryptoApp::new(transport);
+
+        let path = BIP44Path::from_string("m/44'/394'/0'/0/0").unwrap();
+        let resp = app.get_address(&path, true).await.unwrap();
+
+        assert_eq!(resp.public_key.len(), 65);
+
+        let pkhex = hex::encode(&resp.public_key[..]);
+        println!("Public Key   {:?}", pkhex);
+        println!("Address address {:?}", resp.address);
+
+        assert_eq!(pkhex, "048ef50054db1b8c5ff9b02640a25463a37ca7d4249da43b4e6f4ea8f7af70daec5e276294642dec9dc28079397d6962cc32d3909e92995167768fbde7250424d9");
+        assert_eq!(resp.address, "cro1n97t35jymgksmh73mh0zj3qx539k3hg4pfhmncake4ssm3z7rreqzkza53");
+    }
 
     #[async_test]
     #[serial]
@@ -154,7 +153,7 @@ mod integration_tests {
             pkhex,
             "04eda422888bff3b3fa957ab9a509b6ae70c1249c9b9b35f1832aeb2e9a4f94b86076054d2641464e55f85e0c6d27d7dcebd60386f6178dec5e77a2a03330952aa"
         );
-        assert_eq!(resp.address, "D218B4DE1EF21BE0894BB008F3C7D0D3439C174B");
+        assert_eq!(resp.address, "f3c7d0d3439c174b9ce8178c2d2ea95dc1f45c28");
     }
 
     #[async_test]
@@ -167,7 +166,7 @@ mod integration_tests {
         };
         let app = CryptoApp::new(transport);
 
-        let path = BIP44Path::from_string("m/44'/394'/0/0/5").unwrap();
+        let path = BIP44Path::from_string("m/44'/394'/0'/0/5").unwrap();
         let some_message0 = b"";
 
         let response = app.sign(&path, some_message0).await;
